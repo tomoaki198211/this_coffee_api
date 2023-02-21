01b0cdb4d82a01e75a88fc1e181a61ab71e38e70
tomoaki_narukawa <tomo111911111@gmail.com>
2023-02-22 00:03:14 +0900

    seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@ size_starbucks = {
 users = [
   {email: "user1@example.com", password: "password",name: "user1"},
   {email: "user2@example.com", password: "password", name: "user2"},
+  {email: "user3@example.com", password: "password", name: "user3"},
+  {email: "user4@example.com", password: "password", name: "user4"},
+  {email: "user5@example.com", password: "password", name: "user5"},
 ]
 
 users.each do |user|
@@ -33,9 +36,40 @@ categories = [{id: 1, name: "コーヒー(カフェ)"},
 {id: 11, name: "その他"}
 ]
 
-Store.create!(
-  id:1, name:"スターバックス"
-)
+stores = [{id: 1, name: "スターバックスコーヒー"},
+  {id: 2, name: "ドトールコーヒー"},
+  {id: 3, name: "タリーズコーヒー"},
+  {id: 4, name: "コメダ珈琲店"},
+  {id: 5, name: "星乃珈琲店"},
+  {id: 6, name: "サンマルクカフェ"},
+  {id: 7, name: "上島珈琲店"},
+  {id: 8, name: "エクセルシオールカフェ"},
+  {id: 9, name: "カフェ・ベローチェ"},
+  {id: 10, name: "珈琲館"},
+  {id: 11, name: "プロント"},
+  {id: 12, name: "倉式珈琲店"},
+  {id: 13, name: "ホノルルコーヒー"},
+  {id: 14, name: "ディーン・アンド・デルーカ"},
+  {id: 15, name: "猿田彦珈琲"},
+  {id: 16, name: "ベックスコーヒー"},
+  {id: 17, name: "高倉珈琲"},
+  {id: 18, name: "グリーンベリーズコーヒー"},
+  {id: 19, name: "イリーカフェ"},
+  {id: 20, name: "ブルーボトルコーヒー"},
+  {id: 21, name: "カルディコーヒー"},
+  {id: 22, name: "ローソン"},
+  {id: 23, name: "ファミリーマート"},
+  {id: 24, name: "セブンイレブン"},
+  {id: 25, name: "その他"},
+  ]
+
+# ストア作成
+stores.each do |store|
+  Store.create!(
+    id: store[:id],
+    name: store[:name]
+  )
+end
 
 # カテゴリー作成
 categories.each do |category|
